e64ab5c211af19eac303054347d43e46192fa7e0
LastWeekNextDay <doesn@mail.ru>
2024-03-13 01:21:01 +0200

    Client-Side Quiz

--- NonRelational/Pages/Quiz.cshtml.cs
+++ NonRelational/Pages/Quiz.cshtml.cs
@@ -8,17 +8,10 @@ namespace NonRelational.Pages
     public class QuizModel : PageModel
     {
         public ThemeObject SelectedTheme { get; set; }
-        public QuestionObject CurrentQuestion { get; set; }
-        public int CurrentQuestionIndex { get; set; } = 0;
-        public int Score { get; set; } = 0;
-
-        [BindProperty(SupportsGet = true)]
-        public string SelectedAnswer { get; set; }
 
         public void OnGet(string theme)
         {
             SelectedTheme = Global.database.GetTheme(theme);
-            System.Diagnostics.Debug.WriteLine(CurrentQuestionIndex);
         }
     }
 }
