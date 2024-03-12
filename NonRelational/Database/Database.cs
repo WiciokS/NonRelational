550e00a329bb39521edef2ed87b3ef459d1bd6b7
LastWeekNextDay <doesn@mail.ru>
2024-03-12 20:35:07 +0200

    Added minimal DB

--- NonRelational/Database/Database.cs
+++ NonRelational/Database/Database.cs
@@ -1,1 +1,33 @@
-﻿us
+﻿using LiteDB;
+
+namespace NonRelational.Database
+{
+    public class Database
+    {
+        public LiteDatabase LiteDatabase { get; private set; }
+
+        public Database(string connectionString)
+        {
+            LiteDatabase = new LiteDatabase(connectionString);
+            LiteDatabase.GetCollection<ThemeObject>("themes").EnsureIndex(x => x.Theme);
+        }
+
+        public ThemeObject GetTheme(string theme)
+        {
+            var collection = LiteDatabase.GetCollection<ThemeObject>("themes");
+            return collection.FindOne(x => x.Theme == theme);
+        }
+
+        public void InsertTheme(ThemeObject theme)
+        {
+            var collection = LiteDatabase.GetCollection<ThemeObject>("themes");
+            collection.Insert(theme);
+        }
+
+        public void UpdateTheme(ThemeObject theme)
+        {
+            var collection = LiteDatabase.GetCollection<ThemeObject>("themes");
+            collection.Update(theme);
+        }
+    }
+}
--- NonRelational/Database/DatabaseObjects.cs
+++ NonRelational/Database/DatabaseObjects.cs
@@ -3,18 +3,18 @@
     public class AnswerObject
     {
         public string Answer { get; set; }
-        public Boolean IsCorrect { get; set; }
+        public bool IsCorrect { get; set; }
     }
 
     public class QuestionObject
     {
         public string Question { get; set; }
-        public List<AnswerObject> Answers { get; set; }
+        public AnswerObject[] Answers { get; set; }
     }
 
     public class ThemeObject
     {
         public string Theme { get; set; }
-        public List<QuestionObject> Questions { get; set; }
+        public QuestionObject[] Questions { get; set; }
     }
 }
