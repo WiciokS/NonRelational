da454166af7ae1e0c619680be4b995150a9519df
WiciokS <77240194+WiciokS@users.noreply.github.com>
2024-03-12 19:36:58 +0200

    Add project files.

---
+++ NonRelational/Database/DatabaseObjects.cs
@@ -0,0 +1,20 @@
+﻿namespace NonRelational.Database
+{
+    public class AnswerObject
+    {
+        public string Answer { get; set; }
+        public Boolean IsCorrect { get; set; }
+    }
+
+    public class QuestionObject
+    {
+        public string Question { get; set; }
+        public List<AnswerObject> Answers { get; set; }
+    }
+
+    public class ThemeObject
+    {
+        public string Theme { get; set; }
+        public List<QuestionObject> Questions { get; set; }
+    }
+}
