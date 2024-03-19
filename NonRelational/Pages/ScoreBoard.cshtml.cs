e7cec815823c0e13b75ea727b537cd2551ea014f
WiciokS <77240194+WiciokS@users.noreply.github.com>
2024-03-19 21:34:04 +0200

    ...

---
+++ NonRelational/Pages/ScoreBoard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NonRelational.Pages
+{
+    public class ScoreBoardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
